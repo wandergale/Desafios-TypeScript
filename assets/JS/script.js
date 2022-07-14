b6ae852cc802a2ea47f2af831d78d76657e1feba
wandergale <60633445+wandergale@users.noreply.github.com>
2022-07-14 09:41:29 -0300

    Delete assets directory

--- assets/JS/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const employee = {
-    code: 10,
-    name: 'Wanderson'
-};
-const employee2 = {
-    code: 10,
-    name: 'Wanderson'
-};
--- assets/TS/script.ts
+++
@@ -1,9 +0,0 @@
-const employee: { code: number, name: string } = {
-    code: 10,
-    name: 'Wanderson'
-}
-
-const employee2 = {
-    code: 10,
-    name: 'Wanderson'
-}

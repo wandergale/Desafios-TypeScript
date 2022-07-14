47325293fe5008c7339ae1fc1078690f89e64724
wandergale <60633445+wandergale@users.noreply.github.com>
2022-07-14 09:39:08 -0300

    Add files via upload

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+"use strict";
+const employee = {
+    code: 10,
+    name: 'Wanderson'
+};
+const employee2 = {
+    code: 10,
+    name: 'Wanderson'
+};
---
+++ assets/TS/script.ts
@@ -0,0 +1,9 @@
+const employee: { code: number, name: string } = {
+    code: 10,
+    name: 'Wanderson'
+}
+
+const employee2 = {
+    code: 10,
+    name: 'Wanderson'
+}
